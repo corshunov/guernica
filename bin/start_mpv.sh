237a0be09e662eeef5047183e818945fbe07e96c
Aleksandr Korshunov <korshunov.sasha@gmail.com>
2025-05-11 11:51:09 +0100

    Hide eyelids on startup

--- bin/start_mpv.sh
+++ bin/start_mpv.sh
@@ -8,7 +8,7 @@ mpv \
     --aid=no \
     --drm-draw-plane=overlay \
     --drm-drmprime-video-plane=primary \
-    --vf='@over:lavfi="movie=SUN-eyeball-FHD.png[eyeball]; movie=SUN-eyelead-Half-open-FHD.png[halflids]; movie=SUN-eyelead-full-closed-FHD.png[fulllids]; [vid1][eyeball]overlay@eye=x=800:y=390[witheye]; [witheye][halflids]overlay@halflids=x=404:y=340[withhalf]; [withhalf][fulllids]overlay@fulllids=x=404:y=340[vo]"' \
+    --vf='@over:lavfi="movie=SUN-eyeball-FHD.png[eyeball]; movie=SUN-eyelead-Half-open-FHD.png[halflids]; movie=SUN-eyelead-full-closed-FHD.png[fulllids]; [vid1][eyeball]overlay@eye=x=800:y=390[witheye]; [witheye][halflids]overlay@halflids=x=-2000:y=340[withhalf]; [withhalf][fulllids]overlay@fulllids=x=-2000:y=340[vo]"' \
     --input-ipc-server=/tmp/mpvsocket \
     --osd-level=0 \
     #-v \
